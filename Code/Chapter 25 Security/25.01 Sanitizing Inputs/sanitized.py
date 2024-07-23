605682b01a9b6e24e9f9f0849da0b616ea08ce2a
xosuma <xosuma@gmail.com>
2024-07-24 02:34:15 +0900

    fix python spacing

--- Code/Chapter 25 Security/25.01 Sanitizing Inputs/sanitized.py
+++ Code/Chapter 25 Security/25.01 Sanitizing Inputs/sanitized.py
@@ -1,8 +1,9 @@
+import re
 def sanitize(string):
-  # Remove any characters that are not letters or numbers
-  sanitized_string = re.sub(r'[^a-zA-Z0-9]', '', string)
-  
-  return sanitized_string
+    # 문자나 숫자가 아닌 경우 모두 제거합니다.
+    sanitized_string = re.sub(r'[^a-zA-Z0-9]', '', string)
+
+    return sanitized_string
 
 user_input = "abc123!@#"
-print(sanitize(user_input))  # Output: "abc123"
+print(sanitize(user_input))  # 결과: "abc123"
